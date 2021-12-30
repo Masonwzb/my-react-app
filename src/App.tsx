6f95a56bcea69ea84c27a2f143b4fd0f14aa8e3a
Administrator <bineawu@163.com>
2021-12-30 17:39:50 +0800

    feat: initial tic-tac-toe code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Game } from "./ticTacToe/components/Game";
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <br />
+      <Game />
     </div>
   );
 }
---
+++ src/ticTacToe/components/Board.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { Square } from "./Square"
+
+export class Board extends React.Component {
+    renderSquare() {
+        return <Square />
+    }
+
+    render() {
+        const status = 'Next player: X'
+
+        return (
+            <div>
+                <div className="status">{ status }</div>
+                <div className="board-row">
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                </div>
+            </div>
+        )
+    }
+}
